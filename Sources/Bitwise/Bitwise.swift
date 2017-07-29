918befb18d10804fcb8a53075769a48c7ed2b4b4
James <jbean46@gmail.com>
2017-07-29 20:02:50 +0200

    Tighten up

--- Sources/Bitwise/Bitwise.swift
+++ Sources/Bitwise/Bitwise.swift
@@ -13,18 +13,11 @@ private let intBitCount = MemoryLayout<Int>.size * 8
 /// 0x80000000 is 31. This also counts the exponent of the `2` factor in the prime
 /// factorization of a positive number.
 public func countTrailingZeros(_ n: Int) -> Int {
-
     var mask = 1
-
     for index in 0...intBitCount {
-
-        if mask & n != 0 {
-            return index
-        }
-
+        if mask & n != 0 { return index }
         mask <<= 1
     }
-
     return intBitCount
 }
 
@@ -32,18 +25,11 @@ public func countTrailingZeros(_ n: Int) -> Int {
 /// significant one bit. For example, the clz of 0x00F00000 is 8, and the clz of
 /// 0x00000001 is 31.
 public func countLeadingZeros(_ n: Int) -> Int {
-
     var mask = 1 << (intBitCount - 1)
-
     for index in 0...intBitCount {
-
-        if mask & n != 0 {
-            return index
-        }
-
+        if mask & n != 0 { return index }
         mask >>>= 1
     }
-
     return intBitCount
 }
 
