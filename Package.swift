6f5e8eb5dbaf8b17088415fe47d8c140a1114fa9
James <jbean46@gmail.com>
2017-07-31 18:00:46 +0200

    Define library

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "Structure",
+    products: [
+        .library(name: "Structure", targets: ["Structure"])
+    ],
     targets: [
         .target(name: "Structure"),
         .testTarget(name: "StructureTests", dependencies: ["Structure"])
