ff19bba46435b2cafb3bef0491e9e31b0420f0d0
James <jbean46@gmail.com>
2017-08-01 13:09:36 +0200

    Integrate numerical wrappers

---
+++ Sources/StructureWrapping/FloatWrapping.swift
@@ -0,0 +1,128 @@
+//
+//  FloatWrapping.swift
+//  Math
+//
+//  Created by James Bean on 5/10/16.
+//  Copyright © 2016 James Bean. All rights reserved.
+//
+
+import Foundation
+
+// TODO: Add doc comments.
+// - Wrap global functions as static methods in extensions, where possible.
+public protocol FloatWrapping:
+    ExpressibleByFloatLiteral,
+    ExpressibleByIntegerLiteral,
+    Hashable,
+    Comparable
+{
+    init(floatLiteral: Float)
+    init(integerLiteral: Int)
+    var value: Float { get set }
+    init(_ float: Float)
+}
+
+extension FloatWrapping {
+
+    public init(_ float: Float) {
+        self.init(floatLiteral: float)
+    }
+}
+
+
+extension FloatWrapping {
+
+    // MARK: - `Hashable`
+
+    public var hashValue: Int { return value.hashValue }
+}
+
+// MARK: - Comparable
+public func == <T: FloatWrapping>(lhs: T, rhs: T) -> Bool {
+    return lhs.value == rhs.value
+}
+
+public func == <T: FloatWrapping>(lhs: T, rhs: Float) -> Bool {
+    return lhs.value == rhs
+}
+
+public func == <T: FloatWrapping>(lhs: Float, rhs: T) -> Bool {
+    return lhs == rhs.value
+}
+
+public func < <T: FloatWrapping>(lhs: T, rhs: T) -> Bool {
+    return lhs.value < rhs.value
+}
+
+public func < <T: FloatWrapping>(lhs: T, rhs: Float) -> Bool {
+    return lhs.value < rhs
+}
+
+public func < <T: FloatWrapping>(lhs: Float, rhs: T) -> Bool {
+    return lhs < rhs.value
+}
+
+// MARK: - Math
+public func + <T: FloatWrapping>(augend: T, addend: T) -> T {
+    return T(floatLiteral: augend.value + addend.value)
+}
+
+public func + <T: FloatWrapping>(augend: T, addend: T) -> Float {
+    return augend.value + addend.value
+}
+
+public func + <T: FloatWrapping>(lhs: T, rhs: Float) -> Float {
+    return lhs.value + rhs
+}
+
+public func + <T: FloatWrapping>(lhs: Float, rhs: T) -> Float {
+    return lhs + rhs.value
+}
+
+public func - <T: FloatWrapping>(augend: T, addend: T) -> T {
+    return T(floatLiteral: augend.value - addend.value)
+}
+
+public func - <T: FloatWrapping>(minuend: T, subtrahend: T) -> Float {
+    return minuend.value - subtrahend.value
+}
+
+public func - <T: FloatWrapping>(lhs: T, rhs: Float) -> Float {
+    return lhs.value - rhs
+}
+
+public func - <T: FloatWrapping>(lhs: Float, rhs: T) -> Float {
+    return lhs - rhs.value
+}
+
+public func * <T: FloatWrapping>(augend: T, addend: T) -> T {
+    return T(floatLiteral: augend.value * addend.value)
+}
+
+public func * <T: FloatWrapping>(multiplicand: T, multiplier: T) -> Float {
+    return multiplicand.value * multiplier.value
+}
+
+public func * <T: FloatWrapping>(lhs: T, rhs: Float) -> Float {
+    return lhs.value * rhs
+}
+
+public func * <T: FloatWrapping>(lhs: Float, rhs: T) -> Float {
+    return lhs * rhs.value
+}
+
+public func / <T: FloatWrapping>(dividend: T, divisor: T) -> T {
+    return T(floatLiteral: dividend.value / divisor.value)
+}
+
+public func / <T: FloatWrapping>(dividend: T, divisor: T) -> Float {
+    return dividend.value * divisor.value
+}
+
+public func / <T: FloatWrapping>(lhs: T, rhs: Float) -> Float {
+    return lhs.value / rhs
+}
+
+public func / <T: FloatWrapping>(lhs: Float, rhs: T) -> Float {
+    return lhs / rhs.value
+}
---
+++ Sources/StructureWrapping/IntegerWrapping.swift
@@ -0,0 +1,122 @@
+//
+//  IntegerWrapping.swift
+//  Math
+//
+//  Created by James Bean on 1/2/17.
+//  Copyright © 2017 James Bean. All rights reserved.
+//
+
+public protocol IntegerWrapping: ExpressibleByIntegerLiteral, Hashable, Comparable {
+
+    var value: Int { get }
+
+    init(integerLiteral: Int)
+}
+
+extension IntegerWrapping {
+
+    // MARK: - `Equatable`
+
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.value == rhs.value
+    }
+}
+
+extension IntegerWrapping {
+
+    // MARK: - `Comparable`
+
+    public static func < (lhs: Self, rhs: Self) -> Bool {
+        return lhs.value < rhs.value
+    }
+}
+
+extension IntegerWrapping {
+
+    // MARK: - `Hashable`
+
+    public var hashValue: Int {
+        return value.hashValue
+    }
+}
+
+public func == <T: IntegerWrapping>(lhs: T, rhs: Int) -> Bool {
+    return lhs.value == rhs
+}
+
+public func == <T: IntegerWrapping>(lhs: Int, rhs: T) -> Bool {
+    return lhs == rhs.value
+}
+
+public func < <T: IntegerWrapping>(lhs: T, rhs: Int) -> Bool {
+    return lhs.value < rhs
+}
+
+public func < <T: IntegerWrapping>(lhs: Int, rhs: T) -> Bool {
+    return lhs < rhs.value
+}
+
+// MARK: - Math
+public func + <T: IntegerWrapping>(augend: T, addend: T) -> T {
+    return T(integerLiteral: augend.value + addend.value)
+}
+
+public func + <T: IntegerWrapping>(augend: T, addend: T) -> Int {
+    return augend.value + addend.value
+}
+
+public func + <T: IntegerWrapping>(lhs: T, rhs: Int) -> Int {
+    return lhs.value + rhs
+}
+
+public func + <T: IntegerWrapping>(lhs: Int, rhs: T) -> Int {
+    return lhs + rhs.value
+}
+
+public func - <T: IntegerWrapping>(augend: T, addend: T) -> T {
+    return T(integerLiteral: augend.value - addend.value)
+}
+
+public func - <T: IntegerWrapping>(minuend: T, subtrahend: T) -> Int {
+    return minuend.value - subtrahend.value
+}
+
+public func - <T: IntegerWrapping>(lhs: T, rhs: Int) -> Int {
+    return lhs.value - rhs
+}
+
+public func - <T: IntegerWrapping>(lhs: Int, rhs: T) -> Int {
+    return lhs - rhs.value
+}
+
+public func * <T: IntegerWrapping>(augend: T, addend: T) -> T {
+    return T(integerLiteral: augend.value * addend.value)
+}
+
+public func * <T: IntegerWrapping>(multiplicand: T, multiplier: T) -> Int {
+    return multiplicand.value * multiplier.value
+}
+
+public func * <T: IntegerWrapping>(lhs: T, rhs: Int) -> Int {
+    return lhs.value * rhs
+}
+
+public func * <T: IntegerWrapping>(lhs: Int, rhs: T) -> Int {
+    return lhs * rhs.value
+}
+
+public func / <T: IntegerWrapping>(augend: T, addend: T) -> T {
+    return T(integerLiteral: augend.value / addend.value)
+}
+
+public func / <T: IntegerWrapping>(dividend: T, divisor: T) -> Int {
+    return dividend.value * divisor.value
+}
+
+public func / <T: IntegerWrapping>(lhs: T, rhs: Int) -> Int {
+    return lhs.value / rhs
+}
+
+public func / <T: IntegerWrapping>(lhs: Int, rhs: T) -> Int {
+    return lhs / rhs.value
+}
