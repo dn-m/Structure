76040aafd147d36ca0553de78ad3cdda21045665
James <jbean46@gmail.com>
2017-08-02 11:33:29 -0400

    Remove SetAlgebra ~= overload

--- Sources/Structure/PatternMatching.swift
+++ Sources/Structure/PatternMatching.swift
@@ -10,8 +10,3 @@
 public func ~= <S: Sequence> (array: S, value: S.Element) -> Bool where S.Element: Equatable {
     return array.contains(value)
 }
-
-/// - Returns: `true` if the given `set` contains the given `value`.
-public func ~= <S: SetAlgebra> (set: S, value: S.Element) -> Bool {
-    return set.contains(value)
-}
