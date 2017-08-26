dc4886d38a7c572fee66b70ec82ba5665eb9550c
James <jbean46@gmail.com>
2017-08-26 15:54:36 -0400

    Make Pairs a Zip2Sequence over Sequence

--- Sources/Structure/Pairs.swift
+++ Sources/Structure/Pairs.swift
@@ -8,15 +8,17 @@
 
 extension BidirectionalCollection where IndexDistance == Int, Index == Int {
 
-    /// - Returns: Array of 2-tuples comosed of adjacent values.
-    public var pairs: [(Element, Element)] {
-        guard !isEmpty else { return [] }
-        return (0..<count-1).map { (self[$0], self[$0+1]) }
-    }
-
     /// - Returns: An array with same elements as this one, but with the first appended to the end.
     public var wrapped: [Element] {
         guard !isEmpty else { return [] }
         return self + [self[0]]
+    }
+}
+
+extension Sequence where SubSequence: Sequence {
+
+    /// - Returns: `Zip2Sequence` of 2-tuples comosed of adjacent values.
+    public var pairs: Zip2Sequence<Self,SubSequence> {
+        return zip(self,dropFirst())
     }
 }
