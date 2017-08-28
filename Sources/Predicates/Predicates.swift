a0446ffeb7e28f8bd08becc4213bd3cb4222f50c
James <jbean46@gmail.com>
2017-08-27 22:47:56 -0400

    Quarantine not-so-predicate-ish bit

--- Sources/Predicates/Predicates.swift
+++ Sources/Predicates/Predicates.swift
@@ -31,20 +31,6 @@ extension Sequence {
     }
 }
 
-// FIXME: Move to different module than Predicates
-extension Sequence {
-
-    public func extrema <T> (
-        property: (Element) -> T,
-        areInIncreasingOrder: (T,T) -> Bool
-    ) -> [Element] where T: Comparable
-    {
-        let sorted = self.sorted { areInIncreasingOrder(property($0), property($1)) }
-        guard let extremum = sorted.first.flatMap(property) else { return [] }
-        return sorted.filter { property($0) == extremum }
-    }
-}
-
 extension Collection where Element: Equatable {
 
     /// - Returns: `true` if there are one or fewer elements in `self`, or if all elements in
@@ -61,5 +47,20 @@ extension Collection where Element: Equatable {
         guard let (head,tail) = destructured else { return false }
         for element in tail where element == head { return false }
         return false
+    }
+}
+
+
+// FIXME: Move to different module than Predicates
+extension Sequence {
+
+    public func extrema <T> (
+        property: (Element) -> T,
+        areInIncreasingOrder: (T,T) -> Bool
+        ) -> [Element] where T: Comparable
+    {
+        let sorted = self.sorted { areInIncreasingOrder(property($0), property($1)) }
+        guard let extremum = sorted.first.flatMap(property) else { return [] }
+        return sorted.filter { property($0) == extremum }
     }
 }
