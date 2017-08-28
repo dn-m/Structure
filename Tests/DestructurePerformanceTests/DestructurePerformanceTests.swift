57fa54906918405a530e418f03a8520ceaf9e8af
James <jbean46@gmail.com>
2017-08-27 20:24:20 -0400

    Make max an explicit argument label for random int

--- Tests/DestructurePerformanceTests/DestructurePerformanceTests.swift
+++ Tests/DestructurePerformanceTests/DestructurePerformanceTests.swift
@@ -12,14 +12,14 @@ import PerformanceTesting
 
 class DestructurePerformanceTests: PerformanceTestCase {
 
-    func randomInt(_ max: Int) -> Int {
+    func randomInt(max: Int) -> Int {
         return Int(arc4random_uniform(UInt32(max)))
     }
 
     func testDestructure() {
         assertPerformance(.constant) { testPoint in
             meanOutcome {
-                let array = Array(count: testPoint) { randomInt($0) }
+                let array = Array(count: testPoint) { randomInt(max: $0) }
                 return time {
                     let _ = array.destructured
                 }
