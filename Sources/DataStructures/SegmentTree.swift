fab6386cadc63cf7b64727cd625a3e2f8fe8117f
James <jbean46@gmail.com>
2017-12-06 23:59:15 -0500

    Implement SegmentTree more better

--- Sources/DataStructures/SegmentTree.swift
+++ Sources/DataStructures/SegmentTree.swift
@@ -5,34 +5,77 @@
 //  Created by James Bean on 12/4/17.
 //
 
+import Algebra
 import Combinatorics
 import Destructure
 
+/// Interval between two values.
+//
+// FIXME: Move to own file.
 public struct Interval <Metric: SignedNumeric & Comparable> {
 
-    enum Bound {
+    // MARK: - Nested Types
+
+    /// Bounds.
+    public enum Bound {
         case start
         case end
     }
 
+    // MARK: Instance Properties
+
+    /// Start value.
     let start: Metric
+
+    /// End value.
     let end: Metric
 
+    // MARK: - Initializers
+
+    /// Creates an `Interval` with `start` and `end` values.
     public init(_ start: Metric, _ end: Metric) {
         precondition(end >= start)
         self.start = start
         self.end = end
     }
+
+    // MARK: - Instance Methods
+
+    /// - Returns: The intersection of this `Interval` and the given `other`.
+    public func intersection(_ other: Interval) -> Interval? {
+        let start = max(self.start, other.start)
+        let end = min(self.end, other.end)
+        guard end > start && start < end else { return nil }
+        return Interval(start, end)
+    }
+
+    /// - Returns: Whether this `Interval` overlaps with the given `other`.
+    public func overlaps(with other: Interval) -> Bool {
+        return other.start > start || other.end < end
+    }
+
+    /// - Returns: Whether this `Interval` contains the given `offset` including the given `bound`.
+    public func contains(_ offset: Metric, including bound: Bound) -> Bool {
+        let startCompare: (Metric, Metric) -> Bool = bound == .start ? (>=) : (>)
+        let endCompare: (Metric, Metric) -> Bool = bound == .end ? (<=) : (<)
+        return startCompare(offset,start) && endCompare(offset,end)
+    }
 }
 
 extension Interval: Equatable {
+
+    /// - Returns: `true` if intervals are equivalent. Otherwise `false`.
     public static func == (lhs: Interval, rhs: Interval) -> Bool {
         return lhs.start == rhs.start && lhs.end == rhs.end
     }
 }
 
-enum SegmentTree <Metric: SignedNumeric & Comparable> {
+/// Organizaes contiguous intervals into a binary tree structure for efficient searching.
+public enum SegmentTree <Metric: SignedNumeric & Comparable> {
 
+    // MARK: - Instance Properties
+
+    /// The interval of this `SegmentTree`.
     public var interval: Interval<Metric> {
         switch self {
         case .leaf(let interval):
@@ -42,66 +85,103 @@ enum SegmentTree <Metric: SignedNumeric & Comparable> {
         }
     }
 
-    case leaf(Interval<Metric>)
-    indirect case branch(SegmentTree, Interval<Metric>, SegmentTree)
+    /// The leaf intervals contained herein.
+    public var leaves: [Interval<Metric>] {
 
-    init(_ a: SegmentTree, _ b: SegmentTree) {
-        precondition(a.interval.end == b.interval.start)
-        self = .branch(a, Interval(a.interval.start, b.interval.end), b)
+        func flattening(_ tree: SegmentTree, into accum: [Interval<Metric>]) -> [Interval<Metric>] {
+            switch tree {
+            case .branch(let left, _, let right):
+                return left.leaves + right.leaves
+            case .leaf(let interval):
+                return accum + [interval]
+            }
+        }
+
+        return flattening(self, into: [])
     }
 
-    init(_ interval: Interval<Metric>) {
+    // MARK: - Cases
+
+    /// Leaf Interval.
+    case leaf(Interval<Metric>)
+
+    /// Branch Interval containing `SegmentTree` values to the left and right.
+    indirect case branch(SegmentTree, Interval<Metric>, SegmentTree)
+
+    // MARK: - Initializers
+
+    /// Creates a `SegmentTree` with the two given `SegmentTree` values.
+    ///
+    /// - Invariant: The end of the first `SegmentTree` is equal to the start of the second.
+    public init(_ left: SegmentTree, _ right: SegmentTree) {
+        precondition(left.interval.end == right.interval.start)
+        self = .branch(left, Interval(left.interval.start, right.interval.end), right)
+    }
+
+    /// Creates a `SegmentTree` leaf with the given `interval`.
+    public init(interval: Interval<Metric>) {
         self = .leaf(interval)
     }
 
-    init(offsets: [Metric]) {
-
-        func clump(_ trees: [SegmentTree]) -> ([(SegmentTree, SegmentTree)], SegmentTree?) {
-            precondition(!trees.isEmpty)
-            let pairs = stride(from: trees.startIndex, to: trees.endIndex - 1, by: 2).map { index in
-                return (trees[index], trees[index + 1])
-            }
-            let last = trees.count % 2 != 0 ? trees.last! : nil
-            return (pairs,last)
-        }
-
-        func join(_ trees: [SegmentTree]) -> SegmentTree {
-            guard trees.count > 1 else { return trees.first! }
-            let (pairs,last) = clump(trees)
-            let left = join(pairs.map(SegmentTree.init))
-            if let last = last {
-                return SegmentTree(left,last)
-            } else {
-                return left
-            }
-        }
-
-        switch offsets.count {
-        case 0,1:
-            fatalError("Not enough points to create an IntervalTree")
-        default:
-            self = join(offsets.pairs.map(Interval.init).map(SegmentTree.init))
-        }
+    /// Creates a `SegmentTree` from an array of `SegmentTree` values.
+    public init(segmentTrees: [SegmentTree<Metric>]) {
+        precondition(!segmentTrees.isEmpty)
+        self = join(segmentTrees)
     }
 
-    subscript(interval: Interval<Metric>) -> SegmentTree {
-        // first
-        // last?
-        // innnards?
-        // first + innards? + last?
-        fatalError()
+    /// Creates a `SegmentTree` from a given array of `intervals`.
+    public init(intervals: [Interval<Metric>]) {
+        precondition(!intervals.isEmpty)
+        self.init(segmentTrees: intervals.map(SegmentTree.init))
     }
 
-    func interval(containing offset: Metric, including bound: Interval<Metric>.Bound) -> Interval<Metric>? {
+    /// Creates a `SegmentTree` from a given array of `offsets`.
+    ///
+    /// - Invariant: Offsets are in increasing order.
+    public init(offsets: [Metric]) {
+        precondition(offsets.count > 1)
+        self.init(intervals: offsets.pairs.map(Interval.init))
+    }
 
-        let startCompare: (Metric,Metric) -> Bool = bound == .start ? (>=) : (>)
-        let endCompare: (Metric,Metric) -> Bool = bound == .end ? (<=) : (<)
+    // MARK: - Subscripts
+
+    /// - Returns: A new `SegmentTree` from the given `offset` to the end.
+    public subscript (from offset: Metric) -> SegmentTree? {
+        guard offset < interval.end else { return nil }
+        return self[Interval(offset, interval.end)]
+    }
+
+    /// - Returns: A new `SegmentTree` to the given `offset` from the start.
+    public subscript (to offset: Metric) -> SegmentTree? {
+        guard offset > interval.start else { return nil }
+        return self[Interval(interval.start, offset)]
+    }
+
+    /// - Returns: A new `SegmentTree` in the given `interval`.
+    public subscript (interval: Interval<Metric>) -> SegmentTree? {
+        let intervals = leaves.flatMap { $0.intersection(interval) }
+        if intervals.isEmpty { return nil }
+        return SegmentTree(intervals: intervals)
+    }
+
+    // MARK: - Instance Methods
+
+    /// - Returns: The interval containing the given `offset`, including the given `bound`.
+    public func interval(
+        containing offset: Metric,
+        including bound: Interval<Metric>.Bound
+    ) -> Interval<Metric>?
+    {
+        let startCompare: (Metric, Metric) -> Bool = bound == .start ? (>=) : (>)
+        let endCompare: (Metric, Metric) -> Bool = bound == .end ? (<=) : (<)
 
         guard startCompare(offset, interval.start) && endCompare(offset, interval.end) else {
             return nil
         }
 
         switch self {
+        case .leaf(let interval):
+            return interval
         case .branch(let left, let interval, let right):
             if !startCompare(offset, right.interval.start) {
                 return left.interval(containing: offset, including: bound)
@@ -110,13 +190,21 @@ enum SegmentTree <Metric: SignedNumeric & Comparable> {
             } else {
                 return interval
             }
-        case .leaf(let interval):
-            return interval
         }
     }
 }
 
+extension SegmentTree where Metric: Additive {
+
+    /// Creates a `SegmentTree` with the given `lengths`, starting at the `initial` value.
+    public init(lengths: [Metric], from initial: Metric = .zero) {
+        self.init(offsets: lengths.accumulating(initial,+))
+    }
+}
+
 extension SegmentTree: Equatable {
+
+    /// - Returns: `true` if `SegmentTree` values are equivalent. Otherwise, `false`.
     public static func == (lhs: SegmentTree, rhs: SegmentTree) -> Bool {
         switch (lhs,rhs) {
         case let (.leaf(a), .leaf(b)):
@@ -127,4 +215,25 @@ extension SegmentTree: Equatable {
             return false
         }
     }
+}
+
+/// Join a non-empty array of `SegmentTree` values into a single `SegmentTree`.
+///
+/// - Invariant: The given `trees` are contiguous.
+private func join <T> (_ trees: [SegmentTree<T>]) -> SegmentTree<T> {
+
+    /// Groups the given `values` into pairs, with an optional last value in the case that there is
+    /// an odd amount of values.
+    func clump <T> (_ values: [T]) -> ([(T,T)], T?) {
+        let indices = stride(from: values.startIndex, to: values.endIndex - 1, by: 2)
+        let pairs = indices.map { index in (values[index], values[index + 1]) }
+        let last = values.count % 2 != 0 ? values.last! : nil
+        return (pairs,last)
+    }
+
+    precondition(!trees.isEmpty)
+    guard trees.count > 1 else { return trees.first! }
+    let (pairs,last) = clump(trees)
+    let left = join(pairs.map(SegmentTree.init))
+    return last == nil ? left : SegmentTree(left,last!)
 }
--- Tests/DataStructuresTests/SegmentTreeTests.swift
+++ Tests/DataStructuresTests/SegmentTreeTests.swift
@@ -10,6 +10,30 @@ import XCTest
 
 class SegmentTreeTests: XCTestCase {
 
+    func testIntervalContainsTrue() {
+        let interval = Interval(5,10)
+        let offset = 9
+        XCTAssert(interval.contains(offset, including: .start))
+    }
+
+    func testIntervalContainsFalse() {
+        let interval = Interval(5,10)
+        let offset = 12
+        XCTAssertFalse(interval.contains(offset, including: .start))
+    }
+
+    func testIntervalContainsIncludingStart() {
+        let interval = Interval(5,10)
+        let offset = 5
+        XCTAssert(interval.contains(offset, including: .start))
+    }
+
+    func testIntervalContainsIncludingEnd() {
+        let interval = Interval(5,10)
+        let offset = 10
+        XCTAssert(interval.contains(offset, including: .end))
+    }
+
     func testEqualityTrueLeaves() {
         let a = SegmentTree.leaf(Interval(0,5))
         let b = SegmentTree.leaf(Interval(0,5))
@@ -20,22 +44,6 @@ class SegmentTreeTests: XCTestCase {
         let a = SegmentTree.leaf(Interval(0,5))
         let b = SegmentTree.leaf(Interval(1,9))
         XCTAssertNotEqual(a,b)
-    }
-
-    func testEqualityFalseLeafAndBranch() {
-
-    }
-
-    func testEqualityFalseBranchAndLeaf() {
-
-    }
-
-    func testEqualityFalseLeaves() {
-
-    }
-
-    func testEqualityFalseBranches() {
-
     }
     
     func testInitWithOffsetsTwoValues() {
@@ -127,5 +135,72 @@ class SegmentTreeTests: XCTestCase {
         let segmentTree = SegmentTree(offsets: [0,8,11,14,20,23,25,31,45])
         let expected = Interval(14,20)
         XCTAssertEqual(segmentTree.interval(containing: 20, including: .end), expected)
+    }
+
+    func testIntervalsFromNil() {
+        let segmentTree = SegmentTree(offsets: [14,20,23,25,31,45])
+        let offset = 46
+        XCTAssertNil(segmentTree[from: offset])
+    }
+
+    func testIntervalsFromSingle() {
+        let segmentTree = SegmentTree(offsets: [4,9])
+        let offset = 5
+        let result = segmentTree[from: offset]!
+        let expected = SegmentTree(offsets: [5,9])
+        XCTAssertEqual(result, expected)
+    }
+
+    func testIntervalsFromMultiple() {
+        let segmentTree = SegmentTree(offsets: [0,8,11])
+        let offset = 5
+        let result = segmentTree[from: offset]!
+        let expected = SegmentTree(offsets: [5,8,11])
+        XCTAssertEqual(result, expected)
+    }
+
+    func testIntervalsFromMany() {
+        let segmentTree = SegmentTree(offsets: [0,8,11,14,20,23,25,31,45])
+        let offset = 9
+        let result = segmentTree[from: offset]!
+        let expected = SegmentTree(offsets: [9,11,14,20,23,25,31,45])
+        XCTAssertEqual(result, expected)
+    }
+
+    func testIntervalsToMultiple() {
+        let segmentTree = SegmentTree(offsets: [14,20,23,25,31,45])
+        let offset = 25
+        let result = segmentTree[to: offset]!
+        let expected = SegmentTree(offsets: [14,20,23,25])
+        XCTAssertEqual(result, expected)
+    }
+
+    func testIntervalsToMany() {
+        let segmentTree = SegmentTree(offsets: [0,8,11,14,20,23,25,31,45])
+        let offset = 29
+        let result = segmentTree[to: offset]!
+        let expected = SegmentTree(offsets: [0,8,11,14,20,23,25,29])
+        XCTAssertEqual(result, expected)
+    }
+
+    func testIntervalNil() {
+        let segmentTree = SegmentTree(offsets: [0,8,11,14,20,23,25,31,45])
+        XCTAssertNil(segmentTree[Interval(49,50)])
+    }
+
+    func testIntervalSame() {
+        let segmentTree = SegmentTree(offsets: [0,8,11,14,20,23,25,31,45])
+        let interval = Interval(12,13)
+        let result = segmentTree[interval]
+        let expected = SegmentTree(offsets: [12,13])
+        XCTAssertEqual(result, expected)
+    }
+
+    func testInterval() {
+        let segmentTree = SegmentTree(offsets: [0,8,11,14,20,23,25,31,45])
+        let interval = Interval(9,26)
+        let result = segmentTree[interval]
+        let expected = SegmentTree(offsets: [9,11,14,20,23,25,26])
+        XCTAssertEqual(result, expected)
     }
 }
