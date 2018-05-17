9780afa11bb45e25a14968af8ee4557f58473bcf
James <jbean46@gmail.com>
2018-05-17 19:39:08 -0400

    Add RangeProtocol Algebra adapters

--- Sources/DataStructures/RangeProtocol.swift
+++ Sources/DataStructures/RangeProtocol.swift
@@ -5,6 +5,7 @@
 //  Created by James Bean on 5/17/18.
 //
 
+/// Unifying interface for `Range` and `ClosedRange` types.
 public protocol RangeProtocol {
     associatedtype Bound: Comparable
     var lowerBound: Bound { get }
@@ -14,3 +15,19 @@ public protocol RangeProtocol {
 
 extension Range: RangeProtocol { }
 extension ClosedRange: RangeProtocol { }
+
+extension RangeProtocol where Bound: SignedNumeric {
+
+    /// - Returns: The length of this range (upperBound - lowerBound).
+    public var length: Bound {
+        return upperBound - lowerBound
+    }
+}
+
+extension RangeProtocol where Bound: Numeric {
+
+    /// - Returns: A new range equal to this range with bounds shifted by the given amount.
+    public func shifted(by amount: Bound) -> Self {
+        return Self(uncheckedBounds: (lower: lowerBound + amount, upper: upperBound + amount))
+    }
+}
