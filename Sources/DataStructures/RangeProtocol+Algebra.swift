91bddc3ab866c219ff8d53070c41b9937e38bcc2
James <jbean46@gmail.com>
2018-05-17 19:39:08 -0400

    Add RangeProtocol Algebra adapters

---
+++ Sources/DataStructures/RangeProtocol+Algebra.swift
@@ -0,0 +1,26 @@
+//
+//  RangeProtocol+Algebra.swift
+//  DataStructures
+//
+//  Created by James Bean on 5/17/18.
+//
+
+import Algebra
+
+extension ClosedRange: Zero where Bound: Zero {
+    
+    public static var zero: ClosedRange {
+        return ClosedRange(uncheckedBounds: (lower: .zero, upper: .zero))
+    }
+}
+
+extension ClosedRange: AdditiveSemigroup where Bound: Zero {
+
+    public static func + (lhs: ClosedRange, rhs: ClosedRange) -> ClosedRange {
+        let lower = Swift.min(lhs.lowerBound, rhs.lowerBound)
+        let upper = Swift.max(lhs.upperBound, rhs.upperBound)
+        return ClosedRange(uncheckedBounds: (lower: lower, upper: upper))
+    }
+}
+
+extension ClosedRange: Additive where Bound: Zero { }
