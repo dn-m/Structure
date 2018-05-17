49fd5a939f2e14f3bc6dbd80952051485a87e653
James <jbean46@gmail.com>
2018-05-17 19:10:54 -0400

    Remove Bitwise dependency from SumType module

---
+++ Sources/DataStructures/RangeProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  RangeProtocol.swift
+//  DataStructures
+//
+//  Created by James Bean on 5/17/18.
+//
+
+public protocol RangeProtocol {
+    associatedtype Bound: Comparable
+    var lowerBound: Bound { get }
+    var upperBound: Bound { get }
+    init(uncheckedBounds: (lower: Bound, upper: Bound))
+}
