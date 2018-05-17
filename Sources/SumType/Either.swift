0e7e81116f76b9573f1ed4dd70b20e16600f5c1a
James <jbean46@gmail.com>
2018-05-17 01:10:44 -0400

    Make Either Equatable conditionally

--- Sources/SumType/Either.swift
+++ Sources/SumType/Either.swift
@@ -6,8 +6,9 @@
 //
 //
 
-/// Either left type or another
-public enum Either <Left,Right> {
+/// Either of two values.
+public enum Either <Left, Right> {
+
     case left(Left)
     case right(Right)
 
@@ -22,36 +23,17 @@ public enum Either <Left,Right> {
     }
 }
 
-// TODO: Add retroactive Equatable conformance when/if Swift allows it
-public func == <Left: Equatable, Right: Equatable> (
-    lhs: Either<Left,Right>,
-    rhs: Either<Left,Right>
-) -> Bool
-{
-    switch (lhs,rhs) {
-    case let (.left(a), .left(b)):
-        return a == b
-    case let (.right(a), .right(b)):
-        return a == b
-    default:
-        return false
+extension Either: Equatable where Left: Equatable, Right: Equatable {
+
+    /// - Returns: `true` if two `Either` values are equivalent. Otherwise, `false`.
+    public static func == (lhs: Either, rhs: Either) -> Bool {
+        switch (lhs,rhs) {
+        case let (.left(a), .left(b)):
+            return a == b
+        case let (.right(a), .right(b)):
+            return a == b
+        default:
+            return false
+        }
     }
-}
-
-public func != <Left: Equatable, Right: Equatable> (
-    lhs: Either<Left,Right>,
-    rhs: Either<Left,Right>
-) -> Bool
-{
-    return !(lhs == rhs)
-}
-
-public func == <Left: Equatable, Right: Equatable> (
-    lhs: [Either<Left,Right>],
-    rhs: [Either<Left,Right>]
-) -> Bool
-{
-    guard lhs.count == rhs.count else { return false }
-    for (a,b) in zip(lhs,rhs) where a != b { return false }
-    return true
 }
