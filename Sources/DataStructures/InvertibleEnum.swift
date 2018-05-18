e6eef86b3883b98b578315edcc449368c47d811f
James <jbean46@gmail.com>
2018-05-18 14:19:42 -0400

    Make InvertibleEnum.inverse customizable by specifying it in protocol

--- Sources/DataStructures/InvertibleEnum.swift
+++ Sources/DataStructures/InvertibleEnum.swift
@@ -5,9 +5,15 @@
 //  Created by James Bean on 5/17/18.
 //
 
-public protocol InvertibleEnum: CaseIterable, Equatable where Self.AllCases.Index == Int { }
+public protocol InvertibleEnum: CaseIterable, Equatable where Self.AllCases.Index == Int {
+
+    /// - Returns: Inverse of `self`.
+    var inverse: Self { get }
+}
 
 extension InvertibleEnum {
+
+    /// - Returns: Inverse of `self`.
     public var inverse: Self {
         let index = Self.allCases.index(of: self)!
         let inverseIndex = Self.allCases.count - 1 - index
