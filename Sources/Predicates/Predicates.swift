bcabbb07ccf6c04649b638f9723b4789d3d04030
James <jbean46@gmail.com>
2018-06-29 09:22:48 -0700

    Move extrema to SequenceExtensions

--- Sources/Predicates/Predicates.swift
+++ Sources/Predicates/Predicates.swift
@@ -49,15 +49,3 @@ extension Collection where Element: Equatable {
         return false
     }
 }
-
-// FIXME: Move to different module than Predicates
-extension Sequence {
-
-    public func extrema <T> (property: (Element) -> T, areInIncreasingOrder: (T,T) -> Bool)
-        -> [Element] where T: Comparable
-    {
-        let sorted = self.sorted { areInIncreasingOrder(property($0), property($1)) }
-        guard let extremum = sorted.first.flatMap(property) else { return [] }
-        return sorted.filter { property($0) == extremum }
-    }
-}
