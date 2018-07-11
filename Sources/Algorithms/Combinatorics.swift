f210f912a89baf15a03598249906bcafe8571b12
James <jbean46@gmail.com>
2018-07-11 16:45:13 -0700

    Migrate Collection.subsets(cardinality:)

--- Sources/Algorithms/Combinatorics.swift
+++ Sources/Algorithms/Combinatorics.swift
@@ -16,6 +16,27 @@ public func cartesianProduct <T,U> (_ a: T, _ b: U) -> [(T.Element, U.Element)]
 
 extension Collection {
 
+    /// - Returns: All combinations of with a given `cardinality` (i.e., how many elements chosen
+    /// per combination).
+    public func subsets(cardinality k: Int) -> [[Element]] {
+
+        func subsets(cardinality k: Int, appendingTo prefix: [Element], at index: Index)
+            -> [[Element]]
+        {
+            guard k > 0 else { return [prefix] }
+            if index < endIndex {
+                let next = indices.index(after: index)
+                return (
+                    subsets(cardinality: k - 1, appendingTo: prefix + [self[index]], at: next) +
+                    subsets(cardinality: k, appendingTo: prefix, at: next)
+                )
+            }
+            return []
+        }
+
+        return subsets(cardinality: k, appendingTo: [], at: startIndex)
+    }
+
     /// All of the permutations of each of the elements in each of the given sequences.
     public var permutations: [[Element]] {
         func permute <C> (_ values: C) -> [[Element]] where C: Collection, C.Element == Element {
@@ -33,3 +54,5 @@ internal func injecting <C> (_ value: C.Element, into values: C) -> [[C.Element]
     guard let (head, tail) = values.destructured else { return [[value]] }
     return [[value] + values] + injecting(value, into: tail).map { [head] + $0 }
 }
+
+
--- Sources/DataStructures/CollectionExtensions.swift
+++ Sources/DataStructures/CollectionExtensions.swift
@@ -25,30 +25,3 @@ extension Collection where Element: Equatable {
         return false
     }
 }
-
-extension Collection {
-
-    #warning("Move to Algorithms module")
-    /// - Returns: All combinations of with a given cardinality
-    /// (how many elements chosen per combination).
-    public func subsets(cardinality k: Int) -> [[Element]] {
-
-        func subsets(cardinality k: Int, appendingTo prefix: [Element], at index: Index)
-            -> [[Element]]
-        {
-            guard k > 0 else { return [prefix] }
-            if index < endIndex {
-                let next = indices.index(after: index)
-                return (
-                    subsets(cardinality: k - 1, appendingTo: prefix + [self[index]], at: next) +
-                        subsets(cardinality: k, appendingTo: prefix, at: next)
-                )
-            }
-            return []
-        }
-
-        return subsets(cardinality: k, appendingTo: [], at: startIndex)
-    }
-}
-
-
