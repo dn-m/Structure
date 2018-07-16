965357ce60cd5dc1d743fbf3ccca008cf3ec37ad
Ben Wetherfield <bwetherfield@gmail.com>
2018-07-15 23:40:20 -0700

    Implement insert method for BinaryHeap

--- Sources/DataStructures/BinaryHeap.swift
+++ Sources/DataStructures/BinaryHeap.swift
@@ -13,8 +13,16 @@ struct BinaryHeap<Element, Value: Comparable> {
     
     // MARK: - Instance Methods
     
-    mutating func insert(_ element: Element, _ value: Value) {
+    mutating func insert (_ element: Element, _ value: Value) {
         storage.append((element, value))
+        var i = storage.count - 1
+        while (i != 0) {
+            if (storage[i].1 < storage[(i-1)/2].1) {
+                storage.swapAt(i, (i-1)/2)
+                i /= 2
+            }
+            else { break }
+        }
     }
     
     // MARK: - Initializers
