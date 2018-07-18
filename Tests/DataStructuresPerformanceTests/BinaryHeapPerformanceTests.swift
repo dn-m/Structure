fec2698450e00de496dc8c9b332770fd1b31512e
James <jbean46@gmail.com>
2018-07-18 10:15:18 -0700

    Add performance test for pop

--- Tests/DataStructuresPerformanceTests/BinaryHeapPerformanceTests.swift
+++ Tests/DataStructuresPerformanceTests/BinaryHeapPerformanceTests.swift
@@ -11,21 +11,32 @@ import DataStructures
 
 class BinaryHeapPerformanceTests: PerformanceTestCase {
 
+    var random: (element: Int, value: String) {
+        let value = Int.random(in: 1...100)
+        return (value, "\(value)")
+    }
+
     /// Guarantee that insertion into `BinaryHeap` is O(1)
     func testInsert_O_1() {
-
-        var random: (element: Int, value: String) {
-            let value = Int.random(in: 1...100)
-            return (value, "\(value)")
-        }
-
         assertPerformance(.constant) { testPoint in
             meanOutcome {
-                let count = Int.random(in: 1...100)
+                let count = Int.random(in: 1...10_000)
                 var heap = BinaryHeap<Int,String>((0...count).map { _ in random })
                 return time {
                     let (element,value) = random
                     _ = heap.insert(element,value)
+                }
+            }
+        }
+    }
+
+    func testPop_O_1() {
+        assertPerformance(.constant) { testPoint in
+            meanOutcome {
+                let count = Int.random(in: 1...10_000)
+                var heap = BinaryHeap<Int,String>((0...count).map { _ in random })
+                return time {
+                    let _ = heap.pop()
                 }
             }
         }
