379ab12f98c9afc902f7da0e6f23e5d3346da7a9
James <jbean46@gmail.com>
2018-07-18 14:47:36 -0700

    Use test point as size as its supposed to be used

--- Tests/DataStructuresPerformanceTests/BinaryHeapPerformanceTests.swift
+++ Tests/DataStructuresPerformanceTests/BinaryHeapPerformanceTests.swift
@@ -18,10 +18,9 @@ class BinaryHeapPerformanceTests: PerformanceTestCase {
 
     /// Guarantee that insertion into `BinaryHeap` is O(1).
     func testInsert_O_1() {
-        assertPerformance(.constant) { testPoint in
+        assertPerformance(.constant) { size in
             meanOutcome {
-                let count = Int.random(in: 1...10_000)
-                var heap = BinaryHeap<Int,String>((0...count).map { _ in random })
+                var heap = BinaryHeap<Int,String>((0...size).map { _ in random })
                 return time {
                     let (element,value) = random
                     _ = heap.insert(element,value)
@@ -32,10 +31,9 @@ class BinaryHeapPerformanceTests: PerformanceTestCase {
 
     /// Guarantee that popping the minimum value from `BinaryHeap` is O(1).
     func testPop_O_1() {
-        assertPerformance(.constant) { testPoint in
+        assertPerformance(.constant) { size in
             meanOutcome {
-                let count = Int.random(in: 1...10_000)
-                var heap = BinaryHeap<Int,String>((0...count).map { _ in random })
+                var heap = BinaryHeap<Int,String>((0...size).map { _ in random })
                 return time {
                     let _ = heap.pop()
                 }
