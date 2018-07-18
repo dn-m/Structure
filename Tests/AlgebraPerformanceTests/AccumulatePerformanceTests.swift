543859981db015c3ad3ecf2ed6ddd669b57e952a
James <jbean46@gmail.com>
2018-07-18 10:16:17 -0700

    Use Random API in AccumulatePerformanceTests

--- Tests/AlgebraPerformanceTests/AccumulatePerformanceTests.swift
+++ Tests/AlgebraPerformanceTests/AccumulatePerformanceTests.swift
@@ -12,14 +12,10 @@ import PerformanceTesting
 
 class AccumulatePerformanceTests: PerformanceTestCase {
 
-    func randomInt(max: Int) -> Int {
-        return Int(arc4random_uniform(UInt32(max)))
-    }
-
     func testAccumulatingProductLinear() {
         assertPerformance(.linear) { testPoint in
             meanOutcome {
-                let array = Array(count: testPoint) { randomInt(max: $0) }
+                let array = Array(count: testPoint) { Int.random(in: 0..<$0) }
                 return time {
                     _ = array.accumulatingProduct
                 }
