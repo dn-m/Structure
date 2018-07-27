8262510630428da39f41c39a4d25aef6f1d300a1
James <jbean46@gmail.com>
2018-07-26 20:47:57 -0700

    Add doc comments for Either

--- Sources/DataStructures/Either.swift
+++ Sources/DataStructures/Either.swift
@@ -9,14 +9,18 @@
 /// Either of two values.
 public enum Either <Left, Right> {
 
+    /// The left value.
     case left(Left)
+    /// The right value.
     case right(Right)
 
+    /// - Returns: The left value, if it is exists. Otherwise, `nil`.
     public var left: Left? {
         guard case let .left(value) = self else { return nil }
         return value
     }
 
+    /// - Returns: The right value, if it is exists. Otherwise, `nil`.
     public var right: Right? {
         guard case let .right(value) = self else { return nil }
         return value
