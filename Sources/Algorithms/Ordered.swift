1b5f83abc3fc1e853554f902712238332070e421
James <jbean46@gmail.com>
2018-08-09 16:05:42 -0400

    Update doc comment style

--- Sources/Algorithms/Ordered.swift
+++ Sources/Algorithms/Ordered.swift
@@ -5,13 +5,12 @@
 //  Created by James Bean on 8/8/18.
 //
 
-/**
-    let (lower,higher) = ordered(7,3) // => (3,7)
-
- - Note: If both values are equal, they are returned in the order in which they were given
-
- - Returns: 2-tuple of two given values, in order.
- */
+///    let (lower,higher) = ordered(7,3) // => (3,7)
+///
+/// - Note: If both values are equal, they are returned in the order in which they were given
+///
+/// - Returns: 2-tuple of two given values, in order.
+///
 public func ordered <T: Comparable> (_ a: T, _ b: T) -> (T, T) {
     return a <= b ? (a,b) : (b,a)
 }
