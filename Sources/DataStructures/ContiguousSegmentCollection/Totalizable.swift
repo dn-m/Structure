88616efcfc55c7f557836fc6c98c1caf053bf5b0
James <jbean46@gmail.com>
2018-08-24 11:25:52 -0400

    Add doc comments

--- Sources/DataStructures/ContiguousSegmentCollection/Totalizable.swift
+++ Sources/DataStructures/ContiguousSegmentCollection/Totalizable.swift
@@ -5,7 +5,17 @@
 //  Created by James Bean on 8/22/18.
 //
 
+/// Interface for fragment types which can create a fragment version of a whole instance of the
+/// type.
 public protocol Totalizable {
+
+    // MARK: - Associated Types
+
+    /// The type of the given `Whole`.
     associatedtype Whole
+
+    // MARK: - Initializers
+
+    /// Creates a `Totalizable` fragment with an instance of the `Whole` type.
     init(whole: Whole)
 }
