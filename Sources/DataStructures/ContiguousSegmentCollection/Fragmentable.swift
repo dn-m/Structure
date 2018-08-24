4530eb163be0a2e8c0a030d1740e31fcd99653be
James <jbean46@gmail.com>
2018-08-24 17:17:07 -0400

    Remove unused code

--- Sources/DataStructures/ContiguousSegmentCollection/Fragmentable.swift
+++ Sources/DataStructures/ContiguousSegmentCollection/Fragmentable.swift
@@ -29,35 +29,3 @@ extension IntervallicFragmentable where Metric: Additive {
         return fragment(in: range.lowerBound ..< length)
     }
 }
-
-public struct IntervallicFragment <Whole: Fragmentable & Intervallic> {
-    let whole: Whole
-    let range: Range<Whole.Metric>
-    init(whole: Whole, in range: Range<Whole.Metric>) {
-        self.whole = whole
-        self.range = range
-    }
-}
-
-extension IntervallicFragment: IntervallicFragmentable where Whole: Measured & Intervallic {
-    func fragment(in range: Range<Whole.Metric>) -> IntervallicFragment<IntervallicFragment<Whole>> {
-        fatalError()
-    }
-
-
-    public typealias Metric = Whole.Metric
-
-    public var length: Whole.Metric {
-        return range.length
-    }
-
-    public func fragment(in range: Range<Metric>) -> IntervallicFragment {
-        return .init(whole: whole, in: range)
-    }
-
-    public typealias Fragment = IntervallicFragment
-}
-
-extension IntervallicFragment: Fragmentable {
-
-}
