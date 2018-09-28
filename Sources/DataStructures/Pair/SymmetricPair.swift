23e17014261e3b0fc648fcd3dbef062dc633cfdf
James <jbean46@gmail.com>
2018-09-27 21:43:40 -0400

    Add SymmetricPair

---
+++ Sources/DataStructures/Pair/SymmetricPair.swift
@@ -0,0 +1,19 @@
+//
+//  SymmetricPair.swift
+//  DataStructures
+//
+//  Created by James Bean on 9/27/18.
+//
+
+/// A `Pair` in which the two values are of the same type.
+public protocol SymmetricPair: Pair where A == B { }
+
+extension SymmetricPair where A: Equatable {
+
+    /// - Returns: `true` if one of the values contained herein is equivalent to the given `value`.
+    /// Otherwise, `false`.
+    @inlinable
+    public func contains(_ value: A) -> Bool {
+        return a == value || b == value
+    }
+}
