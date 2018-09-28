655a419af2623621431897b6120679d78fbfb938
James Bean <jbean46@gmail.com>
2018-09-28 10:52:51 -0400

    Pull down Queue from dn-m/NotationModel/SpelledPitch (#172)

--- Tests/DataStructuresPerformanceTests/XCTestManifests.swift
+++ Tests/DataStructuresPerformanceTests/XCTestManifests.swift
@@ -7,10 +7,20 @@ extension BinaryHeapPerformanceTests {
     ]
 }
 
+extension QueuePerformanceTests {
+    static let __allTests = [
+        ("testDequeue_O_n", testDequeue_O_n),
+        ("testEnqueue_O_1", testEnqueue_O_1),
+        ("testIsEmpty_O_1", testIsEmpty_O_1),
+        ("testPeek_O_1", testPeek_O_1),
+    ]
+}
+
 #if !os(macOS)
 public func __allTests() -> [XCTestCaseEntry] {
     return [
         testCase(BinaryHeapPerformanceTests.__allTests),
+        testCase(QueuePerformanceTests.__allTests),
     ]
 }
 #endif
