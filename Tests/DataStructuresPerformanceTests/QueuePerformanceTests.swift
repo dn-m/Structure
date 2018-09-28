30a449348116de497041a79680893b2b1d30ad17
James <jbean46@gmail.com>
2018-09-27 22:35:58 -0400

    Add more Queue performance tests

--- Tests/DataStructuresPerformanceTests/QueuePerformanceTests.swift
+++ Tests/DataStructuresPerformanceTests/QueuePerformanceTests.swift
@@ -19,6 +19,8 @@ class QueuePerformanceTests: XCTestCase {
         XCTAssert(benchmark.performance(is: .constant))
     }
 
+    // If this is tested against `.constant`, this test also passes. The tolerance for `.constant`
+    // performance should probably be tightened.
     func testDequeue_O_n() {
         let benchmark = Benchmark.mutating(
             setup: { size -> Queue<Int> in
@@ -28,7 +30,30 @@ class QueuePerformanceTests: XCTestCase {
             },
             measuring: { _ = $0.dequeue() }
         )
-        print(benchmark)
         XCTAssert(benchmark.performance(is: .linear))
+    }
+
+    func testPeek_O_1() {
+        let benchmark = Benchmark.mutating(
+            setup: { size -> Queue<Int> in
+                var q = Queue<Int>()
+                (0..<size).forEach { q.enqueue($0) }
+                return q
+            },
+            measuring: { _ = $0.peek }
+        )
+        XCTAssert(benchmark.performance(is: .constant))
+    }
+
+    func testIsEmpty_O_1() {
+        let benchmark = Benchmark.mutating(
+            setup: { size -> Queue<Int> in
+                var q = Queue<Int>()
+                (0..<size).forEach { q.enqueue($0) }
+                return q
+        },
+            measuring: { _ = $0.isEmpty }
+        )
+        XCTAssert(benchmark.performance(is: .constant))
     }
 }
