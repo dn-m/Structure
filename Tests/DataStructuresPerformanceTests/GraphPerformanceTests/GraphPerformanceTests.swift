07780012199b8eb83c68c0bdfbade9466eb71fe9
James <jbean46@gmail.com>
2018-09-27 23:10:25 -0400

    Refine Queue performance test

---
+++ Tests/DataStructuresPerformanceTests/GraphPerformanceTests/GraphPerformanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  GraphPerformanceTests.swift
+//  DataStructuresTests
+//
+//  Created by James Bean on 9/27/18.
+//
+
+import XCTest
+import PerformanceTesting
+import DataStructures
+
+class GraphPerformanceTests: XCTestCase {
+
+    func testInsertNode_O_1() {
+        
+    }
+}
