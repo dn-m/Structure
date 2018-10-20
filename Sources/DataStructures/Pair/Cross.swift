0bd7339ec616c7b523fbcd8d5371c7be1ab4558d
bwetherfield <bwetherfield@gmail.com>
2018-10-20 16:36:31 -0400

    Implement Cross Comparable conformance (#191)

--- Sources/DataStructures/Pair/Cross.swift
+++ Sources/DataStructures/Pair/Cross.swift
@@ -29,5 +29,15 @@ extension Cross {
     }
 }
 
+extension Cross: Comparable where T: Comparable, U: Comparable {
+    
+    /// - Returns: true if and only if the first element of `lhs` is less than the first element
+    /// of `rhs` OR if those elements are equal and the second element of `lhs` is less than the
+    /// second element of `rhs` (lexicographic ordering).
+    public static func < (lhs: Cross, rhs: Cross) -> Bool {
+        return lhs.a < rhs.a || (lhs.a == rhs.a && lhs.b < rhs.b)
+    }
+}
+
 extension Cross: Equatable where T: Equatable, U: Equatable { }
 extension Cross: Hashable where T: Hashable, U: Hashable { }
