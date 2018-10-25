3033018286381cf054d532c6716837ea9cbcb0bf
James Bean <jbean46@gmail.com>
2018-10-25 00:06:01 -0400

    Make assertion less incorrect for stable sort performance test (#196)

--- Tests/AlgorithmsPerformanceTests/StableSortPerformanceTests.swift
+++ Tests/AlgorithmsPerformanceTests/StableSortPerformanceTests.swift
@@ -12,12 +12,13 @@ import PerformanceTesting
 
 class StableSortPerformanceTests: XCTestCase {
 
-    func testStableSort() {
+    // FIXME: There is currently no linearithmic option for `Complexity`.
+    func testStableSort_O_nlogn() {
         let benchmark = Benchmark.mutating(
             testPoints: Scale.small,
             setup: { Array((0..<$0).map { Int.random(in: 0...$0) }) },
             measuring: { _ = $0.stableSort(<) }
         )
-        XCTAssert(benchmark.performance(is: .linear))
+        XCTAssert(benchmark.performance(is: .quadratic))
     }
 }
