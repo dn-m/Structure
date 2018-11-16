62ae09f1e7d40b66a74e412b8389dbdc6a9688cc
Ben Wetherfield <bwetherfield@gmail.com>
2018-11-15 22:37:05 -0500

    Make compose static

--- Sources/DataStructures/Bimap.swift
+++ Sources/DataStructures/Bimap.swift
@@ -220,7 +220,7 @@ extension Bimap: ExpressibleByDictionaryLiteral {
 
 extension Bimap {
     
-    public func compose <Other: Hashable> (lhs: Bimap, rhs: Bimap<Value, Other>) -> Bimap<Key, Other> {
+    public static func compose <Other: Hashable> (lhs: Bimap, rhs: Bimap<Value, Other>) -> Bimap<Key, Other> {
         return lhs.reduce(into: Bimap<Key,Other>()) { bimap, pair in
                 let (key, value) = pair
                 bimap[key] = rhs[value]
