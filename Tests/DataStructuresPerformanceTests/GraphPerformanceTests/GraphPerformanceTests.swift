5fad035362ac5a5d0989f2809416de122d91883b
Ben Wetherfield <bwetherfield@gmail.com>
2019-06-20 09:24:57 -0700

    Revert scale to small

--- Tests/DataStructuresPerformanceTests/GraphPerformanceTests/GraphPerformanceTests.swift
+++ Tests/DataStructuresPerformanceTests/GraphPerformanceTests/GraphPerformanceTests.swift
@@ -52,7 +52,7 @@ class GraphPerformanceTests: XCTestCase {
 
     func testEdgeFromSourceInCompleteGraph_O_n() {
         let benchmark = Benchmark.mutating(
-            testPoints: Scale.medium,
+            testPoints: Scale.small,
             setup: completeGraph(size:),
             measuring: { _ = $0.edges(from: 0) }
         )
