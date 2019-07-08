f5d490a6d16cb103eae6c0a518126dc6a96c636a
Ben Wetherfield <bwetherfield@gmail.com>
2019-07-08 15:55:29 -0700

    Update PerformanceTesting dependency reference

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "Algorithms", targets: ["Algorithms"])
     ],
     dependencies: [
-        .package(url: "https://github.com/dn-m/PerformanceTesting", .branch("pitchspeller-dependency"))
+        .package(url: "https://github.com/dn-m/PerformanceTesting", from: "0.4.0")
     ],
     targets: [
 
